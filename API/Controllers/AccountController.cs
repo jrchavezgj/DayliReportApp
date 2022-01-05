440ed3b6236597ca4d8b9b8eea8c52006096f655
Jose Chavez <jrchavez.gj@gmail.com>
2022-01-05 00:41:20 -0500

    User Exixt Account Controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,17 +20,21 @@ namespace API.Controllers
          }
 
          [HttpPost("register")]
-         public async Task<ActionResult<AppUser>> Register(string username, string password){
+         public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
              
              using var hmac = new HMACSHA512();
              var user = new AppUser{
-                 UserName = username,
-                 PassordHash= hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                 UserName = registerDto.Username,
+                 PassordHash= hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                  PasswordSalt = hmac.Key
              };
              _context.Users.Add(user);
              await _context.SaveChangesAsync();
              return user;
+         }
+
+         private async Task<bool> UserExist(string username){
+             return await _context.Users.AnyAsync(x = x.UserName == username.ToLower());
          }
     }
 }
